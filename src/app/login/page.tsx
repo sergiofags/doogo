b6edc0c1c04403aa075c969cd0c7e18079d5bf7f
Sérgio Fagundes <contato.sergiofagundes@gmail.com>
2024-06-19 20:15:58 -0300

    update login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,7 +3,16 @@ import Login from '../../components/Login';
 
 const LoginPage = () => {
     return (
-        <Login />
+        <div className="w-screen h-screen flex">
+            <div className="w-1/2 bg-indigo-600 flex justify-center items-center">
+            </div>
+            <div className="w-1/2 h-screen bg-gray-100 flex justify-center items-center">
+                <div className="w-2/4 h-auto bg-white py-12 p-10 bg-white rounded-xl">
+                    <h1 className="mr-4 text-gray-700 font-bold inline-block mb-8 text-3xl">Welcome</h1>
+                    <Login />
+                </div>
+            </div>
+        </div>
     );
 };
 
